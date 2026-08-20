ff45e73520d1d6e680b19b2457f508a09b346a9e
MUsmanFar <musmanfar@example.com>
2026-08-20 15:24:14 +0500

    feat: add Google Tag Manager (GTM-MK34SK6K) scripts to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -74,7 +74,28 @@ export default function RootLayout({
       suppressHydrationWarning
       className={`${plusJakartaSans.variable} ${inter.variable} h-full antialiased`}
     >
+      <head>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MK34SK6K');`}
+        </Script>
+        {/* End Google Tag Manager */}
+      </head>
       <body suppressHydrationWarning className="min-h-full flex flex-col font-sans bg-white text-brand-navy">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MK34SK6K"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-YSJ91HSBK3"
           strategy="afterInteractive"
